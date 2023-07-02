16e22391d2d98539213e9495860a5b37d8f88115
Hui Kang Tong <you@example.com>
2023-07-02 01:40:13 -0700

    work on running python code first

--- echobot.py
+++ echobot.py
@@ -12,7 +12,6 @@ https://bytecodealliance.github.io/wasmtime-py/
 from __future__ import annotations
 
 import os
-import re
 import shutil
 import tempfile
 import uuid
@@ -22,7 +21,6 @@ from sse_starlette.sse import ServerSentEvent
 from wasmtime import Config, Engine, Linker, Module, Store, WasiConfig
 
 from fastapi_poe import PoeBot, run
-from fastapi_poe.client import MetaMessage, stream_request
 from fastapi_poe.types import QueryRequest
 
 TOTAL_FUEL = 10_000_000_000
@@ -108,44 +106,30 @@ def format_output(captured_output, captured_error, fuel_consumed) -> str:
         line = f"\n```error\n{captured_error}\n```"
         lines.append(line)
 
-    line = f"\nApproximate time taken: {fuel_consumed//1_000_000} ms"
+    line = f"Fuel consumed: {fuel_consumed:_}"
     lines.append(line)
 
     return "\n".join(lines)
 
 
-def extract_code(reply):
-    pattern = r"```python([\s\S]*?)```"
-    matches = re.findall(pattern, reply)
-    return "\n\n".join(matches)
+def strip_code(code):
+    if len(code.strip()) < 6:
+        return code
+    code = code.strip()
+    if code.startswith("```") and code.endswith("```"):
+        code = code[3:-3]
+    return code
 
 
 class EchoBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
-        current_message = ""
-        async for msg in stream_request(query, "CheckPythonTool", query.api_key):
-            # Note: See https://poe.com/CheckPythonTool for the prompt
-            if isinstance(msg, MetaMessage):
-                continue
-            elif msg.is_suggested_reply:
-                yield self.suggested_reply_event(msg.text)
-            elif msg.is_replace_response:
-                yield self.replace_response_event(msg.text)
-            else:
-                current_message += msg.text
-                yield self.replace_response_event(current_message)
-
-        code = extract_code(current_message)
-        if code:
-            (
-                captured_output,
-                captured_error,
-                size,
-                data_len,
-                fuel_consumed,
-            ) = await run_code(code)
-            reply_string = format_output(captured_output, captured_error, fuel_consumed)
-            yield self.text_event(reply_string)
+        code = query.query[-1].content
+        code = strip_code(code)
+        captured_output, captured_error, size, data_len, fuel_consumed = await run_code(
+            code
+        )
+        reply_string = format_output(captured_output, captured_error, fuel_consumed)
+        yield self.text_event(reply_string)
 
 
 if __name__ == "__main__":
