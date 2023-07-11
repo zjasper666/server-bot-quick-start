82c2217abe073480d9ce098d72ee7668fb4cb35a
Hui Kang Tong <you@example.com>
2023-07-11 09:54:55 -0700

    working first copy

--- echobot.py
+++ echobot.py
@@ -13,8 +13,13 @@ from fastapi_poe import PoeBot
 from fastapi_poe.types import QueryRequest
 from sse_starlette.sse import ServerSentEvent
 
+import tiktoken
+
+encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
 
 class EchoBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
         last_message = query.query[-1].content
+        tokens = encoding.encode(last_message)
+        last_message = " | ".join([str((encoding.decode_single_token_bytes(token), token))[1:-1] for token in tokens]) 
         yield self.text_event(last_message)
