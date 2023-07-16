c7187aeca9757ecf4edc8baf1cc4e4499d36f3b3
Hui Kang Tong <you@example.com>
2023-07-16 00:05:41 -0700

    working demo

--- echobot.py
+++ echobot.py
@@ -5,58 +5,45 @@ Sample bot that echoes back messages.
 """
 from __future__ import annotations
 
-import random
-import re
-from collections import defaultdict
+import difflib
 from typing import AsyncIterable
-
-from sse_starlette.sse import ServerSentEvent
 
 from fastapi_poe import PoeBot, run
 from fastapi_poe.client import MetaMessage, stream_request
 from fastapi_poe.types import QueryRequest
+from sse_starlette.sse import ServerSentEvent
 
-# introduction to be added https://i.imgur.com/xbXviUO.gif
+LANGUAGE_PROMPT_TEMPLATE = """
+You will follow the instructions from the user and fix the spelling, grammar and improve the style.
 
-EMOJI_INVERSE_MAP = {
-    "https://i.imgur.com/lPHtYl9.gif": ["🔥"],
-    "https://i.imgur.com/aJ9Pnas.gif": ["🧙‍♀️"],
-    "https://i.imgur.com/hgphb9b.gif": ["😱"],
-    "https://i.imgur.com/3YY02tm.gif": ["😱"],
-    "https://i.imgur.com/nx8WjtW.gif": ["🤗"],
-    "https://i.imgur.com/h9vDS5V.gif": ["🙏"],
-    "https://i.imgur.com/RGlKI4T.gif": ["🤩"],
-    "https://i.imgur.com/4KeeXni.gif": ["😎", "🤴", "😠"],
-    "https://i.imgur.com/zJKBaIP.gif": ["🙈"],
-    "https://i.imgur.com/7PzO0Tk.gif": ["😍"],
-    "https://i.imgur.com/WA2STCk.gif": ["👍", "💪"],
-}
+Please fix the following statement.
 
-EMOJI_MAP = defaultdict(list)
+The statement begins.
 
-for image_url, emojis in EMOJI_INVERSE_MAP.items():
-    for emoji in emojis:
-        EMOJI_MAP[emoji].append(image_url)
-
-DEFAULT_EMOJIS = set(EMOJI_MAP.keys())
-CONVERSATION_SUGGESTED_EMOJIS = defaultdict(lambda: DEFAULT_EMOJIS)
-
-ITALICS_PATTERN = r"(?<!\*)\*([^\*]+)\*(?!\*)|(?<!_)_([^_]+)_(?!_)"
-
-EMOJI_PROMPT_TEMPLATE = """
-You will summarize the character reply into one emojis
-
-<user_statement>
+```
 {user_statement}
-</user_statement>
+```
 
-<character_statement>
-{character_statement}
-</character_statement>
+The statement has ended.
 
-This are the available emojis
-{available_emojis}
+Only reply the fixed quoted text. Do not explain.
+Do not begin or end your reply with inverted commas.
 """.strip()
+
+
+def markdown_diff(str1, str2):
+    diff = difflib.ndiff(str1.split(), str2.split())
+    result = []
+
+    for token in diff:
+        if token[0] == "-":
+            result.append(f"~~{token[2:]}~~")  # strikethrough
+        elif token[0] == "+":
+            result.append(f"**{token[2:]}**")  # bold
+        elif token[0] == " ":
+            result.append(token[2:])
+
+    return " ".join(result)
 
 
 class EchoBot(PoeBot):
@@ -64,8 +51,12 @@ class EchoBot(PoeBot):
         user_statement = query.query[-1].content
         print("user_statement", user_statement)
 
+        wrapped_message = LANGUAGE_PROMPT_TEMPLATE.format(user_statement=user_statement)
+        query.query[-1].content = wrapped_message
+        query.query = [query.query[-1]]
+
         character_reply = ""
-        async for msg in stream_request(query, "MeguminHelper", query.api_key):
+        async for msg in stream_request(query, "EnglishDiffTool", query.api_key):
             # Note: See https://poe.com/MeguminHelper for the system prompt
             if isinstance(msg, MetaMessage):
                 continue
@@ -75,48 +66,10 @@ class EchoBot(PoeBot):
                 yield self.replace_response_event(msg.text)
             else:
                 character_reply += msg.text
-                yield self.replace_response_event(character_reply)
-
-        italics_from_character = "\n".join(
-            "".join(element) for element in re.findall(ITALICS_PATTERN, character_reply)
-        )
-
-        character_statement = character_reply
-        if italics_from_character:
-            character_statement = italics_from_character
-            user_statement = ""
-
-        available_emojis = CONVERSATION_SUGGESTED_EMOJIS[query.conversation_id]
-
-        print("character_statement", character_statement)
-        query.query[-1].content = EMOJI_PROMPT_TEMPLATE.format(
-            user_statement=user_statement,
-            character_statement=character_statement,
-            available_emojis=available_emojis,
-        )
-        query.query = [query.query[-1]]
-
-        emoji_classification = ""
-        async for msg in stream_request(query, "EmojiClassifier", query.api_key):
-            # Note: See https://poe.com/EmojiClassifier for the system prompt
-            if isinstance(msg, MetaMessage):
-                continue
-            elif msg.is_suggested_reply:
-                yield self.suggested_reply_event(msg.text)
-            elif msg.is_replace_response:
-                yield self.replace_response_event(msg.text)
-            else:
-                emoji_classification += msg.text
-
-        emoji_classification = emoji_classification.strip()
-        available_emojis.discard(emoji_classification)
-        print("emoji_classification", emoji_classification)
-
-        image_url_selection = EMOJI_MAP.get(emoji_classification)
-        print("image_url_selection", image_url_selection)
-        if image_url_selection:
-            image_url = random.choice(image_url_selection)
-            yield self.text_event(f"\n![{emoji_classification}]({image_url})")
+                rendered_text = markdown_diff(user_statement, character_reply)
+                yield self.replace_response_event(rendered_text)
+        
+        print("character_reply", character_reply)
 
 
 if __name__ == "__main__":
