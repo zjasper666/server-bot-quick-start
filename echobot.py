c90a4812092fef4683a97d8c1a8a9cd99951e513
Hui Kang Tong <you@example.com>
2023-09-07 21:34:36 -0700

    recommend postimages.org

--- echobot.py
+++ echobot.py
@@ -17,7 +17,7 @@ import pytesseract
 import requests
 
 from fastapi_poe import PoeBot, run
-from fastapi_poe.types import QueryRequest
+from fastapi_poe.types import QueryRequest, SettingsResponse
 from PIL import Image
 from sse_starlette.sse import ServerSentEvent
 
@@ -88,15 +88,13 @@ MULTIWORD_FAILURE_REPLY = """\
 Please only send a URL.
 Do not include any other words in your reply.
 
+You can get an image URL by uploading to https://postimages.org/
+
 These are examples of resume the bot can accept.
 
 https://raw.githubusercontent.com/jakegut/resume/master/resume.png
 
-https://pjreddie.com/static/Redmon%20Resume.pdf
-
-https://i.imgur.com/XYyvW6B.png
-
-https://media.discordapp.net/attachments/1070915874996895744/1070915875139506296/jake.jpg
+https://i.postimg.cc/3r0fZ5gy/resume.png
 
 See https://poe.com/huikang/1512927999933968 for an example of an interaction.
 
@@ -105,35 +103,26 @@ You can also try https://poe.com/xyzFormatter for advice specifically on your bu
 
 PARSE_FAILURE_REPLY = """
 I could not load your resume.
-Please check whether the link is publicly accessible.
 
 ---
 
-If you are uploading to Github, please ensure that you are sending something like
-
-https://raw.githubusercontent.com/jakegut/resume/master/resume.png
-
-rather than
-
-https://github.com/jakegut/resume/blob/master/resume.png
-
-To get to raw.githubusercontent.com from github.com, right click "Download" and "Copy link address".
+Please upload your resume to https://postimages.org/ and reply its direct link.
 
 ---
 
-If you are uploading to imgur, please ensure that you are sending something like
+Please ensure that you are sending something like
 
-https://i.imgur.com/XYyvW6B.png
+https://i.postimg.cc/3r0fZ5gy/resume.png
 
 rather than
 
-https://imgur.com/a/6AeB8Vy
-
-To get i.imgur.com from imgur.com, right click the image and "Copy image address".
+https://postimg.cc/LhRVHWQR/9fca0e7d
 
 ---
 
 This bot is not able to accept links from Google drive.
+
+This bot is not able to read images from Imgur.
 
 Remember to redact sensitive information, especially contact details.
 """
@@ -248,6 +237,12 @@ class EchoBot(PoeBot):
             {"role": "assistant", "content": bot_statement}
         )
 
+    async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
+        return SettingsResponse(
+            server_bot_dependencies={"any": 2},
+            allow_attachments=True,
+            introduction_message="Please upload your resume to https://postimages.org/ and reply its direct link."
+        )
 
 if __name__ == "__main__":
     run(EchoBot())
