2bba99a4eb8994cc07e478f32baa57d757562364
Hui Kang Tong <you@example.com>
2023-09-24 12:51:24 +0800

    update deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 for name in "upload" "exec"; do modal deploy helper_"$name".py; done
-for name in "EnglishDiffBot" "LinkAwareBot" "PythonAgent" "matplotlib" "TesseractOCR" "tiktoken" "ResumeReview" "PromotedAnswer" "MeguminWizardEx"; do modal deploy --name "$name" bot_"$name".py; done
+for name in "EnglishDiffBot" "LinkAwareBot" "PythonAgent" "matplotlib" "TesseractOCR" "tiktoken" "ResumeReview" "PromotedAnswer" "MeguminWizardEx"; do modal deploy --name "$name" bot_"$name".py ; curl -X POST https://api.poe.com/bot/fetch_settings/"$name"/$POE_API_KEY; done
