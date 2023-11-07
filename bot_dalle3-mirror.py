cebff2397789f315a7bc323daeecc78d7c5e9222
Hui Kang Tong <you@example.com>
2023-11-06 18:31:04 -0800

    ask ChatGPT to write incremental prompt

--- bot_dalle3-mirror.py
+++ bot_dalle3-mirror.py
@@ -11,12 +11,15 @@ from __future__ import annotations
 import json
 import os
 import time
+import re
 from typing import AsyncIterable
 
 import fastapi_poe.client
 from fastapi_poe import PoeBot, make_app
+from fastapi_poe.client import stream_request
 from fastapi_poe.types import (
     PartialResponse,
+    ProtocolMessage,
     QueryRequest,
     SettingsRequest,
     SettingsResponse,
@@ -29,11 +32,20 @@ fastapi_poe.client.MAX_EVENT_COUNT = 10000
 
 DAY_IN_SECS = 24 * 60 * 60
 
-DAILY_MESSAGE_LIMIT = 100
+# for non-subscribers, the message limit is defined in the bot settings
+SUBSCRIBER_DAILY_MESSAGE_LIMIT = 10
 
 
 stub = Stub("poe-bot-quickstart")
 stub.my_dict = Dict.new()
+
+
+
+def extract_prompt(reply):
+    pattern = r"```prompt([\s\S]*?)```"
+    matches = re.findall(pattern, reply)
+    return ("\n\n".join(matches)).strip()
+
 
 
 def prettify_time_string(second) -> str:
@@ -60,11 +72,27 @@ def prettify_time_string(second) -> str:
     return string
 
 
+user_allowlist = {"u-000000xy92lqvf0s6x4s2mn6so7bu4ye"}
+
+USER_FOLLOWUP_PROMPT = """
+Read my conversation.
+
+Please write a description of the image that I intend to generate.
+
+Put the description inside ```prompt
+"""
+
+
 class EchoBot(PoeBot):
-    async def get_response(self, query: QueryRequest) -> AsyncIterable[ServerSentEvent]:
+    async def get_response(
+        self, request: QueryRequest
+    ) -> AsyncIterable[ServerSentEvent]:
+        print(request.user_id)
+        print(request.query[-1].content)
+
         client = OpenAI()
 
-        dict_key = f"dalle3-mirror-limit-{query.user_id}"
+        dict_key = f"dalle3-mirror-limit-{request.user_id}"
 
         current_time = time.time()
 
@@ -77,7 +105,11 @@ class EchoBot(PoeBot):
         while calls and calls[0] <= current_time - DAY_IN_SECS:
             del calls[0]
 
-        if len(calls) >= DAILY_MESSAGE_LIMIT:
+        if (
+            len(calls) >= SUBSCRIBER_DAILY_MESSAGE_LIMIT
+            and request.user_id not in user_allowlist
+        ):
+            print(request.user_id, len(calls))
             time_remaining = calls[0] + DAY_IN_SECS - current_time
             yield PartialResponse(text=prettify_time_string(time_remaining))
             return
@@ -85,16 +117,33 @@ class EchoBot(PoeBot):
         calls.append(current_time)
         stub.my_dict[dict_key] = calls
 
-        user_statement = query.query[-1].content
+        if len(request.query) > 2:
+            # this is a multi-turn conversation
+            print("len(request)", len(request.query))
+            message = ProtocolMessage(role="user", content=USER_FOLLOWUP_PROMPT)
+            request.query.append(message)
 
-        print(user_statement)
+            inferred_reply = ""
+            async for msg in stream_request(request, "ChatGPT", request.api_key):
+                inferred_reply += msg.text
+
+            instruction = extract_prompt(inferred_reply)
+
+            if not instruction:
+                yield PartialResponse(text=inferred_reply)
+                return
+
+            yield PartialResponse(text=f"```instruction\n{instruction}\n```\n\n")
+        else:
+            instruction = request.query[-1].content
+
+        print(instruction)
         print(stub.my_dict[dict_key])
-        # TODO - get ChatGPT to rewrite the prompt
 
         try:
             response = client.images.generate(
                 model="dall-e-3",
-                prompt=user_statement,
+                prompt=instruction,
                 size="1024x1024",
                 quality="standard",
                 n=1,
@@ -112,12 +161,14 @@ class EchoBot(PoeBot):
         revised_prompt = response.data[0].revised_prompt
         image_url = response.data[0].url
 
+        print(image_url)
+
         yield PartialResponse(text=f"```prompt\n{revised_prompt}\n```\n\n")
         yield PartialResponse(text=f"![image]({image_url})")
 
     async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
         return SettingsResponse(
-            server_bot_dependencies={},
+            server_bot_dependencies={"ChatGPT": 2},
             allow_attachments=False,  # to update when ready
             introduction_message="What do you want to generate with DALL·E 3?",
         )
