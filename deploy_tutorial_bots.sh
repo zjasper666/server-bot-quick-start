fd9bdd279ffa82eeb1d69e01a1cff23b49f175cd
Hui Kang Tong <you@example.com>
2023-12-11 00:52:39 -0800

    fix spelling

--- deploy_tutorial_bots.sh
+++ deploy_tutorial_bots.sh
@@ -1,8 +1,8 @@
 # Script to deploy tutorial bots
 # You need to change the bot handle
 
-# poe.com/EchoBotDemostration
-BOT_NAME="EchoBotDemostration"; modal deploy --name $BOT_NAME echobot.py
+# poe.com/EchoBotDemonstration
+BOT_NAME="EchoBotDemonstration"; modal deploy --name $BOT_NAME echobot.py
 
 # poe.com/CatBotDemo
 BOT_NAME="CatBotDemo"; modal deploy --name $BOT_NAME catbot/__init__.py; curl -X POST https://api.poe.com/bot/fetch_settings/$BOT_NAME/$POE_ACCESS_KEY
@@ -17,5 +17,4 @@ BOT_NAME="ImageResponseBotDemo"; modal deploy --name $BOT_NAME image_response_bo
 BOT_NAME="PDFCounterBotDemo"; modal deploy --name $BOT_NAME pdf_counter_bot.py; curl -X POST https://api.poe.com/bot/fetch_settings/$BOT_NAME/$POE_ACCESS_KEY
 
 # poe.com/TurboVsClaudeBotDemo
-# note: main branch is faulty
 BOT_NAME="TurboVsClaudeBotDemo"; modal deploy --name $BOT_NAME turbo_vs_claude.py; curl -X POST https://api.poe.com/bot/fetch_settings/$BOT_NAME/$POE_ACCESS_KEY
