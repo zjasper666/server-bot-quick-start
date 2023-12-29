a0ce08e03ef590febd06fab9f1afe7680c9b7a64
Hui Kang Tong <you@example.com>
2023-12-29 02:06:26 -0800

    camera ready

--- bot_TrinoAgent.py
+++ bot_TrinoAgent.py
@@ -3,7 +3,7 @@
 BOT_NAME="TrinoAgent"; modal deploy --name $BOT_NAME bot_${BOT_NAME}.py; curl -X POST https://api.poe.com/bot/fetch_settings/$BOT_NAME/$POE_ACCESS_KEY
 
 Test message:
-Show the first row of the nation table
+How to NVL
 
 """
 
@@ -23,12 +23,36 @@ from trino.exceptions import TrinoUserError
 SYSTEM_PROMPT = """
 You are an assistant that helps to write Trino queries.
 
-The user has access to tpch.sf*
+Do NOT use semicolons.
+Limit your results to at most 10 rows.
+Capitalize all Trino keywords.
 
-Do not use semicolons.
+Always show an example by creating tables with some data
 
+```sql
+WITH (insert table name here) (insert comma-separated column names here) AS (
+    -- define the table data here
+)
+SELECT
+-- define the query logic here
+LIMIT 10
+```
+
+Remember to name the columns.
 Enclose the Trino queries with ```sql and ```
-"""
+""".strip()
+
+
+SIMULATED_USER_SUFFIX_PROMPT = """
+Your query has produced the following output
+
+<output>
+{output}
+</output>
+
+If there is an issue, you will fix the Trino query.
+Otherwise, provide a concise comment. Do not repeat the output. Do not mention that that is no issues.
+""".strip()
 
 
 def format_output(columns, rows) -> str:
@@ -60,15 +84,15 @@ def make_query(query):
     try:
         cur.execute(query)
     except TrinoUserError as e:
-        return "```python\n" + str(e) + "\n```"
+        return "```error\n" + e.error_name + "\n" + e.message + "\n```"
     rows = cur.fetchall()
     columns = cur.description
     output = format_output(columns, rows)
     return output
 
 
-class EchoBot(PoeBot):
-    prompt_bot = "GPT-4"
+class TrinoAgentBot(PoeBot):
+    prompt_bot = "ChatGPT"
 
     async def get_response(
         self, request: QueryRequest
@@ -77,11 +101,24 @@ class EchoBot(PoeBot):
             ProtocolMessage(role="system", content=SYSTEM_PROMPT)
         ] + request.query
 
+        request.logit_bias = {
+            "17725": -20,  # (column
+            "20184": -20,  # (col
+            "49430": -20,  # (expression
+            "26": -20,  # ;
+            "280": -20,  # ;\n
+            "45771": 10,  # WITH
+            "3330": -10,  # ' column'
+            "2007": -10,  # ' table'
+            "366": -10, # ' <'
+            "1198": -10, # ' --'
+            "48014": -10, # ' ...)'
+        }
         user_statement = request.query[-1].content
         print("user_statement")
         print(user_statement)
 
-        for _ in range(5):
+        for _ in range(10):
             current_bot_reply = ""
             async for msg in stream_request(request, self.prompt_bot, request.api_key):
                 if isinstance(msg, MetaMessage):
@@ -110,21 +147,30 @@ class EchoBot(PoeBot):
             print(output)
             yield self.text_event(output)
 
-            return
+            yield self.text_event("\n\n\n")
+
+            request.query += [
+                ProtocolMessage(role="bot", content=current_bot_reply),
+                ProtocolMessage(
+                    role="user",
+                    content=SIMULATED_USER_SUFFIX_PROMPT.format(output=output),
+                ),
+            ]
+            print(SIMULATED_USER_SUFFIX_PROMPT.format(output=output))
 
     async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
         return SettingsResponse(
             server_bot_dependencies={self.prompt_bot: 10},
             allow_attachments=False,
             introduction_message=textwrap.dedent(
-                """What do you want to query?"""
+                """Which Trino keyword do you want to learn about?"""
             ).strip(),
         )
 
 
-bot = EchoBot()
+bot = TrinoAgentBot()
 
-image = (
+image_bot = (
     Image.debian_slim()
     .pip_install("fastapi-poe==0.0.23", "trino")
     .env(
@@ -140,7 +186,7 @@ image = (
 stub = Stub("poe-bot-quickstart")
 
 
-@stub.function(image=image)
+@stub.function(image=image_bot)
 @asgi_app()
 def fastapi_app():
     app = make_app(bot, api_key=os.environ["POE_ACCESS_KEY"])
